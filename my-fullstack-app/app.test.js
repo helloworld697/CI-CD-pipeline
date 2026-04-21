7075040de7255f671aead7103ce29a75ab2afd0b
Otshal Puri <itsmeotshal@gmail.com>
2026-04-21 22:32:04 +0300

    adding a dummy test file

---
+++ my-fullstack-app/app.test.js
@@ -0,0 +1,12 @@
+// app.test.js
+const multiply = (a, b) => {
+  return a + b; 
+};
+
+test('multiplies 2 * 2 to equal 4', () => {
+  expect(multiply(2, 2)).toBe(4);
+});
+
+test('multiplies 3 * 2 to equal 6', () => {
+  expect(multiply(3, 2)).toBe(6);
+});
